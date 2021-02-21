e8018571972f581da61da00708fd7e3127da4d30
sylvio pedroza <sylvio.pedroza@gmail.com>
2021-02-21 16:49:42 -0600

    add db system

--- network.tf
+++ network.tf
@@ -2,13 +2,13 @@ data "oci_core_services" "core_services" {}
 
 resource "oci_core_vcn" "network" {
   compartment_id = var.compartment_id
-  display_name   = var.env_name
+  display_name   = var.environment
   freeform_tags  = { "env" = "dev" }
   cidr_block     = var.network_cidr_block
 }
 
 resource "oci_core_subnet" "public" {
-  display_name               = "public-subnet"
+  display_name               = "${var.environment}-public"
   cidr_block                 = var.public_subnet_cidr_block
   compartment_id             = var.compartment_id
   vcn_id                     = oci_core_vcn.network.id
@@ -16,25 +16,91 @@ resource "oci_core_subnet" "public" {
 }
 
 resource "oci_core_subnet" "private" {
-  display_name               = "private-subnet"
+  display_name               = "${var.environment}-private"
   cidr_block                 = var.private_subnet_cidr_block
   compartment_id             = var.compartment_id
   vcn_id                     = oci_core_vcn.network.id
   prohibit_public_ip_on_vnic = true
 }
 
-resource "oci_core_internet_gateway" "test_internet_gateway" {
+resource "oci_core_internet_gateway" "internet_gateway" {
   compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.network.id
+  display_name   = "${var.environment}-internet-gateway"
 }
 
 resource "oci_core_service_gateway" "service_gateway" {
-    compartment_id = var.compartment_id
-    services {
-        service_id = data.oci_core_services.core_services.services.0.id
-    }
-    services {
-        service_id = data.oci_core_services.core_services.services.1.id
-    }
-    vcn_id = oci_core_vcn.network.id
+  compartment_id = var.compartment_id
+  services {
+    service_id = data.oci_core_services.core_services.services.0.id
+  }
+  services {
+    service_id = data.oci_core_services.core_services.services.1.id
+  }
+  vcn_id = oci_core_vcn.network.id
 }
+
+resource "oci_core_network_security_group" "network_security_group" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.network.id
+  display_name   = "${var.environment}-default-secutiry-group"
+}
+
+
+resource "oci_core_route_table" "default" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.network.id
+  display_name   = "${var.environment}-default-route-table"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "oci_core_security_list" "default" {
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.network.id
+  display_name   = "${var.environment}-default-secutiry-list"
+
+  // allow outbound tcp traffic on all ports
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "6"
+  }
+
+  // allow outbound udp traffic on a port range
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "17" // udp
+    stateless   = true
+  }
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "1"
+    stateless   = true
+  }
+
+  // allow inbound ssh traffic from a specific port
+  ingress_security_rules {
+    protocol  = "6" // tcp
+    source    = "0.0.0.0/0"
+    stateless = false
+  }
+
+  // allow inbound icmp traffic of a specific type
+  ingress_security_rules {
+    protocol  = 1
+    source    = "0.0.0.0/0"
+    stateless = true
+  }
+
+  // allow inbound from customer site
+  ingress_security_rules {
+    source   = "0.0.0.0/0"
+    protocol = "6"
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -4,10 +4,11 @@ variable "compartment_id" {
   description = "Compartment ID"
 }
 
-variable "env_name" {
+variable "environment" {
   default = "fcubs-sandbox"
 }
 
+// network
 variable network_cidr_block {
   default = "10.0.0.0/16"
 }
@@ -18,4 +19,61 @@ variable "public_subnet_cidr_block" {
 
 variable "private_subnet_cidr_block" {
   default = "10.0.1.0/24"
+}
+
+// db system
+variable "db_system_shape" {
+  default = "VM.Standard2.2"
+}
+
+variable "db_edition" {
+  default = "ENTERPRISE_EDITION"
+}
+
+variable "db_admin_password" {
+  default = "BAking_#123"
+}
+
+variable "db_version" {
+  default = "19.0.0.0"
+}
+
+variable "db_disk_redundancy" {
+  default = "NORMAL"
+}
+
+variable "sparse_diskgroup" {
+  default = true
+}
+
+variable "hostname" {
+  default = "fcubsdb"
+}
+
+variable "host_user_name" {
+  default = "opc"
+}
+
+variable "n_character_set" {
+  default = "AL16UTF16"
+}
+
+variable "character_set" {
+  default = "AL32UTF8"
+}
+
+variable "db_workload" {
+  default = "OLTP"
+}
+
+variable "data_storage_size_in_gb" {
+  default = "256"
+}
+
+variable "license_model" {
+  default = "LICENSE_INCLUDED"
+}
+
+variable "node_count" {
+  default = "1"
 }
