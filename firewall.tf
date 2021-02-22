64a78e521b2b84774a92eddd73dd029d1647b44a
sylvio pedroza <sylvio.pedroza@gmail.com>
2021-02-21 18:13:35 -0600

    TCP

--- firewall.tf
+++ firewall.tf
@@ -8,7 +8,7 @@ resource "oci_core_network_security_group_security_rule" "public_db_ingress" {
   network_security_group_id = oci_core_network_security_group.public_db_sg.id
   description               = "allow-external-to-db"
   direction                 = "INGRESS"
-  protocol                  = "all"
+  protocol                  = 6 //TCP
   stateless                 = false
 
   source_type = "CIDR"
