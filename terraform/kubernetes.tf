d6890d7c7e7bdc0fc80698de1ff56689a41c7c8d
sylvio pedroza <sylvio.pedroza@gmail.com>
2021-02-27 12:28:11 -0600

    is_pod_security_policy_enabled=false

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -19,7 +19,7 @@ resource "oci_containerengine_cluster" "cluster" {
 
     admission_controller_options {
       #Optional
-      is_pod_security_policy_enabled = true
+      is_pod_security_policy_enabled = false
     }
 
     kubernetes_network_config {
