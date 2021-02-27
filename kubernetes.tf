c925555bfa4df1554fd473508d07b8cec0d7230f
sylvio pedroza <sylvio.pedroza@gmail.com>
2021-02-26 18:02:41 -0600

    add node pool

--- kubernetes.tf
+++ kubernetes.tf
@@ -28,3 +28,32 @@ resource "oci_containerengine_cluster" "cluster" {
     }
   }
 }
+
+resource "oci_containerengine_node_pool" "node_pool" {
+  #Required
+  cluster_id         = oci_containerengine_cluster.cluster.id
+  compartment_id     = var.compartment_id
+  kubernetes_version = data.oci_containerengine_node_pool_option.test_node_pool_option.kubernetes_versions[0]
+  name               = "tfPool"
+  node_shape         = "VM.Standard2.1"
+  subnet_ids         = [oci_core_subnet.public.id]
+
+  #Optional
+  initial_node_labels {
+    #Optional
+    # key   = "cluster"
+    # value = "value"
+  }
+
+  node_source_details {
+    #Required
+    image_id    = local.oracle_linux_images.0
+    source_type = "IMAGE"
+
+    #Optional
+    boot_volume_size_in_gbs = "60"
+  }
+
+  quantity_per_subnet = 1
+  ssh_public_key      = var.ssh_public_key
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 locals {
   full_name = "fcubs-${var.environment}"
+  all_sources = "${data.oci_containerengine_node_pool_option.test_node_pool_option.sources}"
+  oracle_linux_images = [for source in local.all_sources : source.image_id if length(regexall("Oracle-Linux-[0-9]*.[0-9]*-20[0-9]*",source.source_name)) > 0]
 }
 
 // environment
